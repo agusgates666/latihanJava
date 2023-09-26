a9ed23f585d10ffd78fb38b671512040f0a3a948
agusgates666 <135308532+agusgates666@users.noreply.github.com>
2023-09-27 00:54:06 +0700

    Add files via upload

---
+++ Agus.java
@@ -0,0 +1,27 @@
+package projectBaru;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author heckkkeerr
+ */
+public class Agus {
+    public static final String NAMA = "Agus";
+    public static final String DBDRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DBCONNECTION = "jdbc:mysql://localhost:3306/tokobuku";
+    public static final String DBUSER = "root";
+    public static final String DBPASS = "";
+    
+    public static Connection db() {
+        try {
+            Class.forName(DBDRIVER);
+            return DriverManager.getConnection(DBCONNECTION,DBUSER,DBPASS);
+        }catch(Exception e) {
+            return null;
+        }
+
+    }
+
+}
---
+++ ProjectBaru.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectbaru;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author hekeerrrr
+ */
+public class ProjectBaru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     //   new FormUtama().setVisible(true);
+         Connection connection = null;
+        try {
+            // buat koneksi ke server mysql.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/tokobuku", "root", "");
+
+            // siapkan objek statement untuk query
+            Statement statement;
+            statement = connection.createStatement();
+            ResultSet resultSet;
+
+            // eksekusi query
+            resultSet = statement.executeQuery("select * from barang");
+
+            // baca dan tampilkan data dari query
+            String kode;
+            String nama;
+            while (resultSet.next()) { // baca data & geser kursor ke record selanjutnya
+                kode = resultSet.getString("kode"); // baca kolom "kode"
+                nama = resultSet.getString("nama"); // baca kolom "nama"
+                System.out.println("kode : " + kode + " nama : " + nama); // tampilkan data
+            }
+
+            // hapus objek dan koneksi
+            resultSet.close();
+            statement.close();
+            connection.close();
+        }
+        catch (Exception exception) {
+            // tampilkan pesan error (jika terjadi kesalahan)
+            System.out.println(exception);
+        }
+    }
+    }
+    
+
